75d2199c9a4f6179edac35abf8a38e95b5d96566
xingyuanwang <wangxingyuan@parkingwang.com>
2020-09-05 12:35:45 +0800

    注册对公告字段处理的回调函数

--- app/model/model.go
+++ app/model/model.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/maogou/ginapi/global"
 	"github.com/maogou/ginapi/pkg/setting"
+	"time"
 )
 
 //所有model的公共属性
@@ -41,8 +42,78 @@ func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 	}
 
 	db.SingularTable(true)
+	//替换现有回调的顺序
+	db.Callback().Create().Replace("gorm:update_time_stamp",updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp",updateTimeStampForUpdateCallback)
+	db.Callback().Delete().Replace("gorm:delete",deleteCallback)
+
 	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
 	db.DB().SetMaxOpenConns(databaseSetting.MaxOpenConns)
 
 	return db, nil
+}
+
+//新增行为的回调
+func updateTimeStampForCreateCallback(scope *gorm.Scope){
+	if !scope.HasError() {
+		nowTime := time.Now().Unix()
+		//scope.FieldByName 获取档期是否包含所需字段
+		if createTimeField,ok := scope.FieldByName("CreatedOn");ok {
+			//IsBlank判断该字段是否为空
+			if createTimeField.IsBlank {
+				//如为空给该值设置值
+				_ = createTimeField.Set(nowTime)
+			}
+		}
+
+		if modifyTimeField,ok := scope.FieldByName("ModifiedOn");ok {
+			if modifyTimeField.IsBlank {
+				_ = modifyTimeField.Set(nowTime)
+			}
+		}
+	}
+}
+
+//更新的行为回调
+func updateTimeStampForUpdateCallback(scope *gorm.Scope)  {
+	//scope.Get()来获取档期设置的标识gorm:update_column的值
+	if _,ok := scope.Get("gorm:update_column");ok{
+		//如果没有设置update_column 则为ModifiedOn字段设置值
+		_  = scope.SetColumn("ModifiedOn",time.Now().Unix())
+	}
+}
+
+//删除的行为回调
+func deleteCallback(scope *gorm.Scope)  {
+	if !scope.HasError() {
+		var extraOption string
+		if str,ok := scope.Get("gorm:delete_option");ok {
+			extraOption = fmt.Sprint(str)
+		}
+
+		deletedOnField,hasDeletedField := scope.FieldByName("DeletedOn")
+		isDelField,hasIsDelField := scope.FieldByName("IsDel")
+		if !scope.Search.Unscoped && hasDeletedField && hasIsDelField {
+			now := time.Now().Unix()
+			scope.Raw(fmt.Sprintf(
+				"UPDATE %v SET %v=%v,%v=%v%v%v",
+				//获取当前的表名
+				scope.QuotedTableName(),
+				scope.Quote(deletedOnField.DBName),
+				scope.AddToVars(now),
+				scope.Quote(isDelField.DBName),
+				scope.AddToVars(1),
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extraOption),
+				)).Exec()
+		}
+	}
+}
+
+func addExtraSpaceIfExist(str string) string  {
+	if str != "" {
+		return " " + str
+	}
+
+	return str
 }
