b01c579f169794848022eb2edc17de5f64cf4d5e
wangxingyuan <wangxingyuan@parkingwang.com>
2021-01-31 13:47:28 +0800

    增加sql语句

---
+++ docs/blog.sql
@@ -0,0 +1,41 @@
+CREATE TABLE `blog_tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT '' COMMENT '标签名称',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT '删除时间',
+  `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0为未删除 1为已删除',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用 1为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标签管理';
+
+
+CREATE TABLE `blog_article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT '' COMMENT '文章标题',
+  `desc` varchar(255) DEFAULT '' COMMENT '文章简述',
+  `cover_image_url` varchar(255) DEFAULT '' COMMENT '文章封面图',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT '删除时间',
+  `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0为未删除 1为已删除',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0为禁用 1为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章表';
+
+CREATE TABLE `blog_article_tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL COMMENT '文章id',
+  `tag_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '标签id',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT '删除时间',
+  `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0为未删除 1为已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章标签关联表';
