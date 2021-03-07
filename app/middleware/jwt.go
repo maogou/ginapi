bfe06babbb9880e80e933fdc0d284fa1ecca7793
maogou <kinyou_xy@126.com>
2021-03-07 10:25:15 +0800

    增加jwt中间件

---
+++ app/middleware/jwt.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/maogou/ginapi/pkg/app"
+	"github.com/maogou/ginapi/pkg/errcode"
+)
+
+//token中间件
+func Jwt() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var (
+			token string
+			ecode = errcode.Success
+		)
+
+		if queryToken, exist := c.GetQuery("token"); exist {
+			token = queryToken
+		} else {
+			token = c.GetHeader("token")
+		}
+
+		if token == "" {
+			ecode = errcode.InvalidParams
+		} else {
+			_, err := app.ParseToken(token)
+
+			if err != nil {
+				//断言错误
+				switch err.(*jwt.ValidationError).Errors {
+				case jwt.ValidationErrorExpired:
+					ecode = errcode.UnauthorizedTokenTimeout
+				default:
+					ecode = errcode.UnauthorizedTokenError
+				}
+			}
+		}
+
+		if ecode != errcode.Success {
+			response := app.NewResponse(c)
+			response.ToErrorResponse(ecode)
+			c.Abort()
+			return
+		}
+
+		//执行下一个中间件
+		c.Next()
+	}
+}
--- routes/api.go
+++ routes/api.go
@@ -25,30 +25,31 @@ func NewRouter() *gin.Engine {
 	tag := v1.Tag{}
 	auth := v1.JwtAuth{}
 
-	v1 := router.Group("/api/v1")
+	//不需要鉴权的路由
+	router.POST("/api/v1/token", auth.GetAuth)
+
+	//需要鉴权的路由
+	apiV1 := router.Group("/api/v1").Use(middleware.Jwt())
 	{
 		// 创建标签
-		v1.POST("/tags", tag.Create)
+		apiV1.POST("/tags", tag.Create)
 		// 删除指定标签
-		v1.DELETE("/tags/:id", tag.Delete)
+		apiV1.DELETE("/tags/:id", tag.Delete)
 		// 更新指定标签
-		v1.PUT("/tags/:id", tag.Update)
+		apiV1.PUT("/tags/:id", tag.Update)
 		// 获取标签列表
-		v1.GET("/tags", tag.List)
+		apiV1.GET("/tags", tag.List)
 
 		// 创建文章
-		v1.POST("/articles", article.Create)
+		apiV1.POST("/articles", article.Create)
 		// 删除指定文章
-		v1.DELETE("/articles/:id", article.Delete)
+		apiV1.DELETE("/articles/:id", article.Delete)
 		// 更新指定文章
-		v1.PUT("/articles/:id", article.Update)
+		apiV1.PUT("/articles/:id", article.Update)
 		// 获取指定文章
-		v1.GET("/articles/:id", article.Get)
+		apiV1.GET("/articles/:id", article.Get)
 		// 获取文章列表
-		v1.GET("/articles", article.List)
-
-		//jwt
-		v1.POST("/token", auth.GetAuth)
+		apiV1.GET("/articles", article.List)
 	}
 
 	return router
